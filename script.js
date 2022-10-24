94b797b5a71b2fcb81fac8784303f8d24ebfe85f
DIERGENE <diergene@gmail.com>
2022-10-24 15:48:38 -0300

    fist commit

--- script.js
+++ script.js
@@ -11,5 +11,27 @@ function nextImage(){
         count = 1;
     }
 
- document.getElementById("radio"+count).checked = true;
+document.getElementById("radio"+count).checked = true;
 }
+
+//..login..//
+
+function toggleResetPswd(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle() // display:block or none
+    $('#logreg-forms .form-reset').toggle() // display:block or none
+}
+
+function toggleSignUp(e){
+    e.preventDefault();
+    $('#logreg-forms .form-signin').toggle(); // display:block or none
+    $('#logreg-forms .form-signup').toggle(); // display:block or none
+}
+
+$(()=>{
+    // Login Register Form
+    $('#logreg-forms #forgot_pswd').click(toggleResetPswd);
+    $('#logreg-forms #cancel_reset').click(toggleResetPswd);
+    $('#logreg-forms #btn-signup').click(toggleSignUp);
+    $('#logreg-forms #cancel_signup').click(toggleSignUp);
+})
